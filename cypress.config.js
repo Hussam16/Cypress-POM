f5bd6430015941afe990a6334b50226f7765904e
hossam <hossamabdelfattah15@gmail.com>
2022-12-26 20:17:02 +0200

    add latest ignore file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'wopnj1',
+  projectId: "nkivgv",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
